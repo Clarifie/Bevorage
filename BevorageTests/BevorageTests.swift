9166cd30906e20bb8060be353216578638162be3
Clarifie <friendlyneighborhoodemailbot1@gmail.com>
2025-07-10 13:38:49 -0400

    Initial Commit

---
+++ BevorageTests/BevorageTests.swift
@@ -0,0 +1,17 @@
+//
+//  BevorageTests.swift
+//  BevorageTests
+//
+//  Created by Owen Hickman on 7/10/25.
+//
+
+import Testing
+@testable import Bevorage
+
+struct BevorageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
